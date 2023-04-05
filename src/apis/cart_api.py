49fbc2661215eb5a3d8191244cc3adbe6517c75d
Daniel <plam.developer@gmail.com>
2023-04-05 20:52:51 +0200

    3.0.1
    Hotfix of a navigation bar.

--- src/apis/cart_api.py
+++ src/apis/cart_api.py
@@ -1,4 +1,3 @@
-import json
 from datetime import datetime
 from uuid import uuid4
 
@@ -11,7 +10,8 @@ cart_api_blueprint = Blueprint('cart_api_blueprint', __name__)
 
 @cart_api_blueprint.route('/cart', methods=['GET'])
 def get_cart_items():
-    return jsonify(session.get('cart_items', []))
+    cart_items = session.get('cart_items', [])
+    return jsonify(cart_items)
 
 
 @cart_api_blueprint.route('/cart', methods=['POST'])
@@ -28,31 +28,19 @@ def add_to_cart():
     cart_items.append(item)
     session['cart_items'] = cart_items
 
-    cart_items_json = json.dumps(cart_items)
-    return cart_items_json, 200
+    return jsonify(cart_items), 200
 
 
 @cart_api_blueprint.route('/cart/<item_name>', methods=['DELETE'])
 def remove_item_from_cart(item_name):
     item_name = item_name.replace('-', ' ')
     cart_items = session.get('cart_items', [])
-    item_removed = False
     for i, item in enumerate(cart_items):
         if item['name'] == item_name:
             del cart_items[i]
-            item_removed = True
-            break
-    if item_removed:
-        session['cart_items'] = cart_items
-        return redirect('/payment')
-    else:
-        return jsonify({'error': 'Item not found'}), 404
-
-
-@cart_api_blueprint.route('/cart', methods=['DELETE'])
-def clear_cart():
-    session.pop('cart_items', None)
-    return '', 204
+            session['cart_items'] = cart_items
+            return redirect('/payment')
+    return jsonify({'error': 'Item not found'}), 404
 
 
 @cart_api_blueprint.route('/cart/checkout', methods=['POST'])
@@ -61,7 +49,8 @@ def checkout():
     order_items = session.get('cart_items', [])
 
     try:
-        order_item_dicts = [{'name': item['name'], 'description': item['description'], 'price': item['price']} for item in order_items]
+        order_item_dicts = [{'name': item['name'], 'description': item['description'], 'price': item['price']}
+                            for item in order_items]
         order = Order.create(id=order_id, created_at=datetime.utcnow(), order_items=order_item_dicts)
         session['order_id'] = order.id
         session.pop('cart_items', None)
@@ -69,5 +58,3 @@ def checkout():
         return jsonify({'error': str(e)}), 400
 
     return redirect("/orders")
-
-
